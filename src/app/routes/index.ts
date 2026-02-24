c983013d635603a956523b64abf3354b01fc76dc
Captain-Kanak <kanakroy835@gmail.com>
2026-02-24 21:20:11 +0600

    create doctor successfully

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { SpecialityRoutes } from "../modules/speciality/speciality.route";
 import { AuthRoutes } from "../modules/auth/auth.route";
+import { UserRoutes } from "../modules/user/user.route";
 
 const router: Router = Router();
 
 router.use("/auth", AuthRoutes);
+
+router.use("/users", UserRoutes);
 
 router.use("/specialities", SpecialityRoutes);
 
