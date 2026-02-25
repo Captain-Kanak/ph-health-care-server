c06062a80ea4f90d15c026f9dc2dfd8517c353d0
Captain-Kanak <kanakroy835@gmail.com>
2026-02-25 20:47:01 +0600

    handle zod error globally

--- src/app/middleware/error-handler.ts
+++ src/app/middleware/error-handler.ts
@@ -2,6 +2,12 @@ import type { NextFunction, Request, Response } from "express";
 import { AppError } from "../utils/AppError";
 import { env } from "../../config/env";
 import status from "http-status";
+import * as z from "zod";
+
+interface ErrorSourceType {
+  path: string;
+  message: string;
+}
 
 function globalErrorHandler(
   err: Error,
@@ -13,6 +19,7 @@ function globalErrorHandler(
     console.error(err);
   }
 
+  const errorSource: ErrorSourceType[] = [];
   let statusCode: number = status.INTERNAL_SERVER_ERROR;
   let message: string = err.message || "Internal Server Error";
 
@@ -21,12 +28,23 @@ function globalErrorHandler(
     message = err.message;
   }
 
+  if (err instanceof z.ZodError) {
+    statusCode = status.BAD_REQUEST;
+    message = "Zod Validation Error";
+
+    err.issues.forEach((issue) => {
+      errorSource.push({
+        path: issue.path.join("."),
+        message: issue.message,
+      });
+    });
+  }
+
   return res.status(statusCode).json({
     success: false,
     message,
-    ...(env.NODE_ENV === "development" && {
-      stack: err.stack,
-    }),
+    errorSource,
+    ...(env.NODE_ENV === "development" && { stack: err.stack }),
   });
 }
 
--- src/app/validation/params.zod.ts
+++ src/app/validation/params.zod.ts
@@ -1,5 +1,5 @@
 import * as z from "zod";
 
 export const paramsIdZodSchema = z.object({
-  id: z.uuid("Id is required"),
+  id: z.uuid("UUID is invalid or missing"),
 });
