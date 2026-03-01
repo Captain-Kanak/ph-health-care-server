9f8a33215e3c736ae3d6c79bca33aa588fb0b47b
Captain-Kanak <kanakroy835@gmail.com>
2026-03-01 20:23:18 +0600

    successfully create admin and doctor

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { UserController } from "./user.controller";
-import { createDoctorZodSchema } from "../../validation/doctor.zod";
 import { validateRequestBody } from "../../middleware/zod-middleware";
 import authMiddleware from "../../middleware/auth-middleware";
 import { UserRole } from "@prisma/client";
@@ -18,7 +17,7 @@ router.post(
 router.post(
   "/create-doctor",
   authMiddleware(UserRole.SUPER_ADMIN, UserRole.ADMIN),
-  validateRequestBody(createDoctorZodSchema),
+  validateRequestBody(UserValidation.CreateDoctorZodSchema),
   UserController.createDoctor,
 );
 
--- src/app/validation/doctor.zod.ts
+++ src/app/validation/doctor.zod.ts
@@ -1,51 +1,6 @@
 import * as z from "zod";
 import { Gender } from "@prisma/client";
 
-const createDoctorZodSchema = z.object({
-  password: z
-    .string("Password is required")
-    .min(8, "Password must be at least 8 characters long")
-    .max(50, "Password can't be longer than 50 characters"),
-  doctor: z.object({
-    name: z
-      .string("Name is required")
-      .max(50, "Name can't be longer than 50 characters"),
-    email: z
-      .email("Invalid email address")
-      .max(100, "Email can't be longer than 100 characters"),
-    image: z.url("Invalid URL format").optional(),
-    phone: z
-      .string()
-      .min(11, "Phone number must be at least 11 characters long")
-      .max(14, "Phone number can't be longer than 14 characters")
-      .optional(),
-    address: z
-      .string()
-      .max(500, "Address can't be longer than 500 characters")
-      .optional(),
-    registrationNumber: z
-      .string("Registration number is required")
-      .max(50, "Registration number can't be longer than 50 characters"),
-    experience: z
-      .number("Experience is required")
-      .min(2, "Experience must be at least 2 years"),
-    gender: z.enum([Gender.MALE, Gender.FEMALE, Gender.OTHER]),
-    appointmentFee: z.number("Appointment fee is required"),
-    qualification: z
-      .string("Qualification is required")
-      .max(500, "Qualification can't be longer than 500 characters"),
-    currentWorkingPlace: z
-      .string("Current working place is required")
-      .max(500, "Current working place can't be longer than 500 characters"),
-    designation: z
-      .string("Designation is required")
-      .max(500, "Designation can't be longer than 500 characters"),
-  }),
-  specialities: z
-    .array(z.uuid(), "Specialities are required")
-    .min(1, "At least one speciality is required"),
-});
-
 const UpdateDoctorZodSchema = z
   .object({
     name: z.string().max(50, "Name can't be longer than 50 characters"),
@@ -74,4 +29,4 @@ const UpdateDoctorZodSchema = z
   })
   .partial();
 
-export { createDoctorZodSchema, UpdateDoctorZodSchema };
+export { UpdateDoctorZodSchema };
